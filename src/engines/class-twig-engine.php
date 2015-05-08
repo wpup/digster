231ed23465f3836516b8000d03f281a9ac352cc5
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2015-05-08 22:53:31 +0200

    Trying to fix travis..

--- src/engines/class-twig-engine.php
+++ src/engines/class-twig-engine.php
@@ -34,17 +34,10 @@ class Twig_Engine extends Engine {
 
     protected function add_extension( \Twig_ExtensionInterface $extension ) {
         $env  = $this->env_instance();
-        $exts = $env->getExtensions();
-
-        if ( isset( $exts[$extension->getName()] ) ) {
-            return;
-        }
 
         try {
             $env->addExtension( $extension );
-        } catch( LogicException $e ) {
-            return;
-        }
+        } catch( LogicException $e ) {}
     }
 
     /**
