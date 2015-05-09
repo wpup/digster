cd51eae42ace09a1d7b21e4f6178f1df5bfbedf3
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2015-05-09 11:47:42 +0200

    Added tests for after_setup_theme hook

--- tests/class-digster-test.php
+++ tests/class-digster-test.php
@@ -9,12 +9,23 @@ namespace Digster\Tests;
 class Digster_Test extends \WP_UnitTestCase {
 
     /**
-     * The action `plugins_loaded` should have the `boilerplate` hook
+     * The action `plugins_loaded` should have the `digster` hook
      * and should have a default priority of 0.
      */
 
-    public function testPluginsLoadedAction() {
+    public function test_plugins_loaded_action() {
         $this->assertEquals( 0, has_action( 'plugins_loaded', 'digster' ) );
     }
+
+	/**
+	 * Test `setup_actions` method.
+	 */
+
+	public function test_setup_actions() {
+		$plugin_loader = \Digster\Plugin_Loader::instance();
+
+		// Test `after_setup_theme` action.
+		$this->assertEquals( 10, has_action( 'after_setup_theme', [$plugin_loader, 'load_extensions'] ) );
+	}
 
 }
