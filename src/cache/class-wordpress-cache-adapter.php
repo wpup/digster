a47f0901bebebf460104c801cfbf742b737ac6a3
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2015-05-20 23:10:54 +0200

    Fixed spaces to tabs

--- src/cache/class-wordpress-cache-adapter.php
+++ src/cache/class-wordpress-cache-adapter.php
@@ -6,74 +6,74 @@ use Asm89\Twig\CacheExtension\CacheProviderInterface;
 
 class WordPress_Cache_Adapter implements CacheProviderInterface  {
 
-    /**
-     * The group key.
-     *
-     * @var string
-     */
+	/**
+	 * The group key.
+	 *
+	 * @var string
+	 */
 
-    private $group;
+	private $group;
 
-    /**
-     * The key prefix.
-     *
-     * @var string
-     */
+	/**
+	 * The key prefix.
+	 *
+	 * @var string
+	 */
 
-    private $key_prefix;
+	private $key_prefix;
 
-    /**
-     * Constructor.
-     *
-     * @param string $key_prefix
-     * @param string $group
-     */
+	/**
+	 * Constructor.
+	 *
+	 * @param string $key_prefix
+	 * @param string $group
+	 */
 
-    public function __construct( $key_prefix = '', $group = '' ) {
-        $this->key_prefix = $key_prefix;
-        $this->group = $group;
+	public function __construct( $key_prefix = '', $group = '' ) {
+		$this->key_prefix = $key_prefix;
+		$this->group = $group;
 
-        if ( function_exists( 'wp_cache_add_global_groups' ) ) {
-            wp_cache_add_global_groups( [$group] );
-        }
-    }
+		if ( function_exists( 'wp_cache_add_global_groups' ) ) {
+			wp_cache_add_global_groups( [$group] );
+		}
+	}
 
-    /**
-     * Fetch cache data.
-     *
-     * @param string $key
-     *
-     * @return mixed
-     */
+	/**
+	 * Fetch cache data.
+	 *
+	 * @param string $key
+	 *
+	 * @return mixed
+	 */
 
-    public function fetch( $key ) {
-        $key = $this->get_key( $key );
-        return wp_cache_get( $key, $this->group );
-    }
+	public function fetch( $key ) {
+		$key = $this->get_key( $key );
+		return wp_cache_get( $key, $this->group );
+	}
 
-    /**
-     * Get key with key prefix.
-     *
-     * @param string
-     *
-     * @return string
-     */
+	/**
+	 * Get key with key prefix.
+	 *
+	 * @param string
+	 *
+	 * @return string
+	 */
 
-    private function get_key( $key ) {
-        return $this->key_prefix . $key;
-    }
+	private function get_key( $key ) {
+		return $this->key_prefix . $key;
+	}
 
-    /**
-     * Save cache data.
-     *
-     * @param string $key
-     * @param mixed $value
-     * @param int $expire
-     */
+	/**
+	 * Save cache data.
+	 *
+	 * @param string $key
+	 * @param mixed $value
+	 * @param int $expire
+	 */
 
-    public function save( $key, $value, $expire = 0 ) {
-        $key = $this->get_key( $key );
-        wp_cache_set( $key, $value, $this->group, $expire );
-    }
+	public function save( $key, $value, $expire = 0 ) {
+		$key = $this->get_key( $key );
+		wp_cache_set( $key, $value, $this->group, $expire );
+	}
 
 }
