521d58aab40b71779fab24df7a8b0ec9b7b75bf2
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2015-08-16 22:44:00 +0200

    Make view method public

--- src/class-factory.php
+++ src/class-factory.php
@@ -171,7 +171,7 @@ class Factory {
      *
      * @return string
      */
-    protected function view( $view ) {
+    public function view( $view ) {
         if ( preg_match( '/\.\w+$/', $view, $matches ) && in_array( $matches[0], $this->extensions() ) ) {
             return str_replace( '.', '/', preg_replace( '/' . $matches[0] . '$/', '', $view ) ) . $matches[0];
         }
