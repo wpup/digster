56433b59c9c7b3879adc5d1c3490bc7267b8b6e3
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2015-08-16 22:43:51 +0200

    Add array access support and magic method to view class

--- src/class-view.php
+++ src/class-view.php
@@ -2,9 +2,10 @@
 
 namespace Digster;
 
+use ArrayAccess;
 use Digster\Engines\Engine;
 
-class View {
+class View implements ArrayAccess {
 
     /**
      * The view engine instance.
@@ -54,33 +55,31 @@ class View {
      * @return array
      */
     protected function gather_data() {
-        $data    = array_merge( $this->factory->get_composer( $this ), $this->data );
-        $data    = array_merge( $this->factory->get_shared(), $data );
-        $result  = $data;
+        $data = array_merge( $this->factory->get_composer( $this ), $this->data );
+        $data = array_merge( $this->factory->get_shared(), $data );
 
         foreach ( $data as $index => $callback ) {
             if ( is_callable( $callback ) ) {
-                $value = call_user_func( $callback, $result );
+                $value = call_user_func( $callback, $this );
             } else {
                 $value = $callback;
             }
 
             if ( is_array( $value ) ) {
-                $this->data = $result = $value;
+                $this->data = $value;
             } else if ( is_string( $value ) && class_exists( $value ) ) {
 				$class = new $value();
 				$class->compose( $this );
-                $result = array_merge( $result, $this->data );
             }
         }
 
-        foreach ( $result as $key => $value ) {
+        foreach ( $this->data as $key => $value ) {
             if ( $value instanceof View ) {
-                $result[$key] = $value->render();
+                $this->data[$key] = $value->render();
             }
         }
 
-        return $result;
+        return $this->data;
     }
 
     /**
@@ -96,13 +95,19 @@ class View {
      * Add a view instance to the view data.
      *
      * @param string $key
-     * @param string $view
+     * @param string $view_name
      * @param array $data
      *
      * @return $this
      */
-    public function nest( $key, $view, array $data = [] ) {
-        return $this->with( $key, $this->factory->make( $view, $data ) );
+    public function nest( $key, $view_name, array $data = [] ) {
+        $view = $this->factory->make( $view_name, $data );
+
+        if ( $view->get_name() === $this->factory->view( $view_name ) ) {
+            return $this;
+        }
+
+        return $this->with( $key, $view );
     }
 
     /**
@@ -111,7 +116,11 @@ class View {
      * @return string
      */
     public function render() {
-        return $this->engine->render( $this->view, $this->gather_data() );
+        if ( $this->factory->exists( $this->view ) ) {
+            return $this->engine->render( $this->view, $this->gather_data() );
+        }
+
+        return '';
     }
 
     /**
@@ -143,6 +152,96 @@ class View {
         } catch ( \Exception $e ) {
             return '';
         }
+    }
+
+	/**
+	 * Determine if key exists in view data.
+	 *
+	 * @param string $key
+	 *
+	 * @return bool
+	 */
+	// @codingStandardsIgnoreStart
+	public function offsetExists( $key ) {
+	// @codingStandardsIgnoreEnd
+		return array_key_exists( $key, $this->data );
+	}
+
+	/**
+	 * Get value from view data.
+	 *
+	 * @param string $key
+	 *
+	 * @return mixed
+	 */
+	// @codingStandardsIgnoreStart
+	public function offsetGet( $key ) {
+	// @codingStandardsIgnoreEnd
+		return $this->data[$key];
+	}
+
+	/**
+	 * Set a value to the view data.
+	 *
+	 * @param string $key
+	 * @param mixed $value
+	 */
+	// @codingStandardsIgnoreStart
+	public function offsetSet( $key, $value ) {
+	// @codingStandardsIgnoreEnd
+	   $this->with( $key, $value );
+    }
+
+	/**
+	 * Unset value from view data.
+	 *
+	 * @param string $id
+	 */
+	// @codingStandardsIgnoreStart
+	public function offsetUnset( $key ) {
+	// @codingStandardsIgnoreEnd
+		unset( $this->data[$key] );
+	}
+
+	/**
+	 * Get value from view data.
+	 *
+	 * @param string $key
+	 *
+	 * @return mixed
+	 */
+    public function &__get( $key ) {
+        return $this->data[$key];
+    }
+
+	/**
+	 * Determine if key exists in view data.
+	 *
+	 * @param string $key
+	 *
+	 * @return bool
+	 */
+    public function __isset( $key ) {
+        return array_key_exists( $key, $this->data );
+    }
+
+	/**
+	 * Set a value to the view data.
+	 *
+	 * @param string $key
+	 * @param mixed $value
+	 */
+    public function __set( $key, $value ) {
+        $this->with( $key, $value );
+    }
+
+	/**
+	 * Unset value from view data.
+	 *
+	 * @param string $id
+	 */
+    public function __unset( $key ) {
+        unset( $this->data[$key] );
     }
 
 }
