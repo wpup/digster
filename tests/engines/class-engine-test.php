65bacb3718c1a9316be4ff834804ddb3dd85a016
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2015-10-04 20:36:31 +0200

    Add config and extensions tests

---
+++ tests/engines/class-engine-test.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Frozzare\Tests\Digster;
+
+use Frozzare\Digster\Digster;
+
+class Engine_Test extends \WP_UnitTestCase {
+
+    public function test_config() {
+        $engine = Digster::factory()->engine();
+        $engine->config( 'name', 'Fredrik' );
+        $this->assertSame( $engine->config( 'name' ), 'Fredrik' );
+
+        $engine->config( [
+            'one' => 1,
+            'two' => 2
+        ] );
+        $this->assertSame( $engine->config( 'one' ), 1 );
+        $this->assertSame( $engine->config( 'two' ), 2 );
+    }
+
+}
--- tests/engines/class-twig-engine-test.php
+++ tests/engines/class-twig-engine-test.php
@@ -7,6 +7,15 @@ use Frozzare\Digster\Engines\Twig_Engine;
 
 class Twig_Engine_Test extends \WP_UnitTestCase {
 
+    public function test_add_extensions() {
+        require_once __DIR__ . '/../fixtures/class-test-extensions.php';
+        $engine = Digster::factory()->engine();
+        $out = $engine->add_extension( new \Test_Extensions );
+        $this->assertNull( $out );
+        $out = $engine->add_extension( new \Test_Extensions );
+        $this->assertNull( $out );
+    }
+
     public function test_engine() {
         $loader = new \Twig_Loader_Array( [
             'index.html' => 'Hello, {{ name }}!'
@@ -29,6 +38,17 @@ class Twig_Engine_Test extends \WP_UnitTestCase {
 
         $output = Digster::fetch( 'admin.profile.html', ['name' => 'Fredrik'] );
         $this->assertSame( 'Hello, Fredrik!', $output );
+    }
+
+    public function test_register_extensions() {
+        require_once __DIR__ . '/../fixtures/class-test-extensions.php';
+        $engine = Digster::factory()->engine();
+        $out = $engine->register_extensions( new \Test_Extensions );
+        $this->assertNull( $out );
+        $out = $engine->register_extensions( '\\Test_Extensions' );
+        $this->assertNull( $out );
+        $out = $engine->register_extensions( [new \Test_Extensions, new \Test_Extensions] );
+        $this->assertNull( $out );
     }
 
 }
