d1a5afaa53c57f9c59b03a03b6e899d1a9044dfa
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2015-10-05 07:54:49 +0200

    Add translation test

--- tests/extensions/class-i18n-extensions-test.php
+++ tests/extensions/class-i18n-extensions-test.php
@@ -40,4 +40,29 @@ class I18n_Extensions_Test extends \WP_UnitTestCase {
         $this->assertSame( 'Hello, world!', $output );
     }
 
+    public function test_trans_3() {
+        add_action( 'my_action', function () {
+            echo 'world';
+        } );
+
+        $loader = new \Twig_Loader_Array( [
+            'index.html' => 'Hello, {% trans "world" %}!'
+        ] );
+
+        $engine = Digster::factory()->engine();
+        $engine->set_loader( $loader );
+
+        $output = Digster::fetch( 'index.html' );
+
+        $this->assertSame( 'Hello, world!', $output );
+
+        tests_add_filter( 'gettext', function ( $translated_text, $text, $domain ) {
+            return $text === 'world' ? 'världen' : $text;
+        }, 20, 3 );
+
+        $output = Digster::fetch( 'index.html' );
+
+        $this->assertSame( 'Hello, världen!', $output );
+    }
+
 }
