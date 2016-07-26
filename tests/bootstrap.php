87d37128735c25e773477f9ba0ec6862fa31596c
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2016-07-26 22:00:24 +0200

    Remove load of digster from bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,8 +3,5 @@
 // Load test data file.
 WP_Test_Suite::load_files( __DIR__ . '/test-data.php' );
 
-// Load Digster loader file as a plugin.
-WP_Test_Suite::load_plugins( __DIR__ . '/../digster.php' );
-
 // Run the WordPress test suite.
 WP_Test_Suite::run();
