7e2f8c19442282b6e26f1ae9e7683c067eb747ce
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2016-08-18 12:22:00 +0200

    Load digster

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,5 +6,8 @@ require_once __DIR__ . '/../vendor/autoload.php';
 // Load test data file.
 WP_Test_Suite::load_files( __DIR__ . '/test-data.php' );
 
+// Load Digster.
+WP_Test_Suite::load_plugins( __DIR__ . '/../digster.php' );
+
 // Run the WordPress test suite.
 WP_Test_Suite::run();
